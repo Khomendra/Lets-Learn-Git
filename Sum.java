42d400e2dc05210a42a8b9066ee443cd8ab00d0f
Khomendra <khomendrakumar.ak.94@gmail.com>
2023-02-09 16:35:01 +0530

    Added files in git repo

--- Sum.java
+++ Sum.java
@@ -2,7 +2,7 @@ public class Sum{
     public static void main(String[] args){
         int a = 5;
         int b = 6;
-
+        System.out.println("Addition");
         System.out.println("Sum: "+(a+b));
     }
 }
